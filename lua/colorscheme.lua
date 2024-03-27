6ac01ab1184d62d96d73ac1be884a3aaf1b09f9b
leslie-lau <804738464@qq.com>
2024-03-27 14:27:00 +0800

    feat: add gruvbox cfg

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,10 +1,30 @@
-vim.cmd([[ if has('termguicolors')
-            set termguicolors
-            endif
-]])
+-- Default options:
+require("gruvbox").setup({
+  terminal_colors = true, -- add neovim terminal colors
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = true,
+    emphasis = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = false,
+})
 
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd('colorscheme gruvbox')
 vim.g.airline_theme = 'base16_gruvbox_dark_pale'
 -- vim.base16colorspace = 256
-vim.cmd([[let g:everforest_background = 'hard']])
