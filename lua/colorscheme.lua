56f636e844f9133ceba702add4bb128f0c75c8c6
leslie-lau <804738464@qq.com>
2024-05-24 15:13:27 +0800

    perf: del unused plugin (everforest, because i don't like it 0-0)
    add proto support for treesitter

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -27,4 +27,4 @@ require("gruvbox").setup({
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd('colorscheme gruvbox')
 vim.g.airline_theme = 'base16_gruvbox_dark_pale'
--- vim.base16colorspace = 256
+vim.base16colorspace = 256
