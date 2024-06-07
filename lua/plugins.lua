39cba5ded6664a9c9d63f3a2b8d98e0318601dea
Leslie-Lau <804738464@qq.com>
2024-06-07 23:17:27 +0800

    feat: add theme ayu

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -39,6 +39,8 @@ return require('packer').startup(function(use)
         -- NOTE: PUT YOUR THIRD PLUGIN HERE --
         ---------------------------------------
         use { "ellisonleao/gruvbox.nvim" }
+        -- theme ayu
+        use 'Shatur/neovim-ayu'
         use 'vim-airline/vim-airline'
         use 'vim-airline/vim-airline-themes'
         use {
