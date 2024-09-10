bee8fbff962dcee30a3d5ac3e67dd6024954ee92
leslie-lau <804738464@qq.com>
2024-09-10 17:32:04 +0800

    feat: add relativenumber cfg

--- lua/options.lua
+++ lua/options.lua
@@ -11,6 +11,7 @@ vim.opt.expandtab = true -- tabs are spaces, mainly because of python
 
 -- UI config
 vim.opt.number = true -- show absolute number
+vim.wo.relativenumber = true -- show relative number
 vim.opt.cursorline = true -- highlight cursor line underneath the cursor horizontally
 vim.opt.splitbelow = true -- open new vertical split bottom
 vim.opt.splitright = true -- open new horizontal splits right
