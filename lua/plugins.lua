19302f16dd5faf727dafc0b80eccd022b6f0d87f
leslie-lau <804738464@qq.com>
2024-10-12 12:14:11 +0800

    chore: change theme from ayu to matrix

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -39,8 +39,7 @@ return require('packer').startup(function(use)
         -- NOTE: PUT YOUR THIRD PLUGIN HERE --
         ---------------------------------------
         use { "ellisonleao/gruvbox.nvim" }
-        -- theme ayu
-        use 'Shatur/neovim-ayu'
+        use 'iruzo/matrix-nvim'
         use 'vim-airline/vim-airline'
         use 'vim-airline/vim-airline-themes'
         use {
