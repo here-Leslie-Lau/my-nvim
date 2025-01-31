47deab619bf5591f5953ec6d5611d57dbbebb9e9
Leslie-Lau <804738464@qq.com>
2025-01-31 13:33:19 +0800

    feat: add prettier command

--- lua/options-plugins.lua
+++ lua/options-plugins.lua
@@ -89,3 +89,6 @@ vim.cmd([[ let g:copilot_filetypes = {
                               \ '*': v:true,
                               \ }
 ]])
+
+-- prettier
+vim.cmd([[command! -nargs=0 Prettier :CocCommand prettier.forceFormatDocument]])
