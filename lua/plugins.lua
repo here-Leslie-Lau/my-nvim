db3711f7a29fd49a0fa7bee1491aa9e8b628b5ad
Leslie-Lau <804738464@qq.com>
2025-03-11 14:10:56 +0800

    chore: update the format in lua/plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -18,8 +18,8 @@ local packer_bootstrap = ensure_packer()
 --     <afile> - replaced with the filename of the buffer being manipulated
 vim.cmd([[
   augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  autocmd!
+  autocmd BufWritePost plugins.lua source <afile> | PackerSync
   augroup end
 ]])
 
@@ -32,40 +32,40 @@ vim.cmd([[
 --     ft = string or list,              -- Specifies filetypes which load this plugin.
 --     run = string, function, or table, -- Specify operations to be run after successful installs/updates of a plugin
 return require('packer').startup(function(use)
-        -- Packer can manage itself
-        use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-        ---------------------------------------
-        -- NOTE: PUT YOUR THIRD PLUGIN HERE --
-        ---------------------------------------
-        -- color schemes
-        use { "ellisonleao/gruvbox.nvim" }
+    ---------------------------------------
+    -- NOTE: PUT YOUR THIRD PLUGIN HERE --
+    ---------------------------------------
+    -- color schemes
+    use { "ellisonleao/gruvbox.nvim" }
 
-        -- nvim web devicons
-        use { "nvim-tree/nvim-web-devicons" }
+    -- nvim web devicons
+    use { "nvim-tree/nvim-web-devicons" }
 
-        -- status line
-        use { 'nvim-lualine/lualine.nvim' }
+    -- status line
+    use { 'nvim-lualine/lualine.nvim' }
 
-        -- tree
-        use {
-            'nvim-tree/nvim-tree.lua',
-            cmd = { 'NvimTreeToggle', 'NvimTreeOpen', 'NvimTreeFindFile' },
-            config = function()
-                require('nvim-tree').setup {}
-            end
-        }
+    -- tree
+    use {
+        'nvim-tree/nvim-tree.lua',
+        cmd = { 'NvimTreeToggle', 'NvimTreeOpen', 'NvimTreeFindFile' },
+        config = function()
+            require('nvim-tree').setup {}
+        end
+    }
 
-        -- tree sitter
-        use {
-            'nvim-treesitter/nvim-treesitter',
-            event = 'BufRead',
-            run = function()
+    -- tree sitter
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        event = 'BufRead',
+        run = function()
             local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
             ts_update()
-            end,
-            config = function()
-                require'nvim-treesitter.configs'.setup {
+        end,
+        config = function()
+            require'nvim-treesitter.configs'.setup {
                 -- 安装 language parser
                 -- :TSInstallInfo 命令查看支持的语言
                 ensure_installed = {"vim", "lua", "javascript", "go", "c", "proto"},
@@ -89,62 +89,62 @@ return require('packer').startup(function(use)
                     enable = true
                 }
             }
-            end
-       }
-
-        -- telescope
-        use {
-            'nvim-telescope/telescope.nvim', tag = '0.1.8',
-            cmd = 'Telescope',  -- when run Telescope command, load this plugin
-            requires = { {'nvim-lua/plenary.nvim'} }
-        }
-
-        use {
-            'voldikss/vim-floaterm',
-            cmd = { 'FloatermToggle', 'FloatermNew' },
-        }
-
-        -- code style
-        use {
-            'jiangmiao/auto-pairs',
-            event = 'InsertEnter',  -- enter insert mode to load plugin
-        }
-        use {
-            'lukas-reineke/indent-blankline.nvim',
-            event = 'BufRead',
-            config = function ()
-                require("ibl").setup()
-            end
-        }
-
-        -- code language
-        use {
-            'neoclide/coc.nvim',
-            branch = 'release',
-            event = 'VimEnter',
-        }
-        use {
-            'fatih/vim-go',
-            ft = { 'go' },  -- when open go file, load this plugin
-        }
-
-        use {
-            'nvimdev/dashboard-nvim',
-            event = 'VimEnter',
-            config = function()
-                require('dashboard').setup{}
-            end,
-        }
-
-        -- ai tools
-        use {
-            'github/copilot.vim',
-            event = 'InsertEnter',  -- enter insert mode to load plugin
-        }
-
-        -- Automatically set up your configuration after cloning packer.nvim
-        -- Put this at the end after all plugins
-        if packer_bootstrap then
-            require('packer').sync()
         end
-    end)
+    }
+
+    -- telescope
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        cmd = 'Telescope',  -- when run Telescope command, load this plugin
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    use {
+        'voldikss/vim-floaterm',
+        cmd = { 'FloatermToggle', 'FloatermNew' },
+    }
+
+    -- code style
+    use {
+        'jiangmiao/auto-pairs',
+        event = 'InsertEnter',  -- enter insert mode to load plugin
+    }
+    use {
+        'lukas-reineke/indent-blankline.nvim',
+        event = 'BufRead',
+        config = function ()
+            require("ibl").setup()
+        end
+    }
+
+    -- code language
+    use {
+        'neoclide/coc.nvim',
+        branch = 'release',
+        event = 'VimEnter',
+    }
+    use {
+        'fatih/vim-go',
+        ft = { 'go' },  -- when open go file, load this plugin
+    }
+
+    use {
+        'nvimdev/dashboard-nvim',
+        event = 'VimEnter',
+        config = function()
+            require('dashboard').setup{}
+        end,
+    }
+
+    -- ai tools
+    use {
+        'github/copilot.vim',
+        event = 'InsertEnter',  -- enter insert mode to load plugin
+    }
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
+end)
