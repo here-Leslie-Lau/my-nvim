746b7c2834ffcb8cfc13930574f8285020ead4de
Leslie-Lau <804738464@qq.com>
2025-04-05 18:14:08 +0800

    feat: Update nvim-treesitter config to include TSBufToggle highlight.
    1: Changed opts to config function.
    2: Added TSBufToggle highlight command.

--- lua/plugins/plugin-nvim-treesitter.lua
+++ lua/plugins/plugin-nvim-treesitter.lua
@@ -3,12 +3,17 @@ return {
     lazy = true,
     event = 'BufRead',
     build = ":TSUpdate",
-    opts = {
-        -- 安装 language parser
-        -- :TSInstallInfo 命令查看支持的语言
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go", "gomod", "fish", "javascript" },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-    },
+    config = function()
+        require'nvim-treesitter.configs'.setup{
+            -- 安装 language parser
+            -- :TSInstallInfo 命令查看支持的语言
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go", "gomod", "fish", "javascript" },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+        }
+
+        -- run TSBufToggle highlight
+        vim.cmd('TSBufToggle highlight')
+    end,
 }
