00ade072bbfc5d7cfa7c5a4d23db40f77eaf6465
Leslie-Lau <804738464@qq.com>
2025-04-08 19:54:34 +0800

    feat (plugin-blink-cmp): Change plugin load event to filetype trigger
    1: Comment out 'InsertEnter' event
    2: Add 'markdown' filetype trigger for plugin loading

--- lua/plugins/plugin-blink-cmp.lua
+++ lua/plugins/plugin-blink-cmp.lua
@@ -3,7 +3,8 @@ return {
     -- optional: provides snippets for the snippet source
     dependencies = { 'rafamadriz/friendly-snippets' },
     Lazy = true,
-    event = 'InsertEnter',  -- enter insert mode to load plugin
+    -- event = 'InsertEnter',  -- enter insert mode to load plugin
+    ft = { 'markdown' },
 
     -- use a release tag to download pre-built binaries
     version = '1.*',
