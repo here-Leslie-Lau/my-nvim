4a1d0468951dd6e4f51a43e38b0fd2081a33a6c9
Leslie-Lau <804738464@qq.com>
2025-05-06 13:59:16 +0800

    feat (plugin-copilot): Replace copilot.vim with copilot.lua and configure it extensively.
    1. Replace "github/copilot.vim" with "zbirenbaum/copilot.lua".
    2. Add detailed configuration for copilot.lua.

--- lua/plugins/plugin-copilot.lua
+++ lua/plugins/plugin-copilot.lua
@@ -1,4 +1,84 @@
 return {
-    "github/copilot.vim",
+    -- "github/copilot.vim",
+    "zbirenbaum/copilot.lua",
     event = 'InsertEnter',  -- enter insert mode to load plugin
+    cmd = "Copilot",
+    config = function()
+        require('copilot').setup({
+            panel = {
+                enabled = true,
+                auto_refresh = false,
+                keymap = {
+                    jump_prev = "[[",
+                    jump_next = "]]",
+                    accept = "<CR>",
+                    refresh = "gr",
+                    open = "<M-CR>"
+                },
+                layout = {
+                    position = "bottom", -- | top | left | right | horizontal | vertical
+                    ratio = 0.4
+                },
+            },
+            suggestion = {
+                enabled = true,
+                auto_trigger = true,
+                hide_during_completion = true,
+                debounce = 75,
+                trigger_on_accept = true,
+                keymap = {
+                    accept = "<C-k>",
+                    accept_word = false,
+                    accept_line = false,
+                    next = "<C-n>",
+                    prev = "<C-p>",
+                    dismiss = "<C-]>",
+                },
+            },
+            filetypes = {
+                yaml = false,
+                markdown = false,
+                help = false,
+                gitcommit = false,
+                gitrebase = false,
+                hgcommit = false,
+                svn = false,
+                cvs = false,
+                ["."] = false,
+            },
+            auth_provider_url = nil, -- URL to authentication provider, if not "https://github.com/"
+            logger = {
+                file = vim.fn.stdpath("log") .. "/copilot-lua.log",
+                file_log_level = vim.log.levels.OFF,
+                print_log_level = vim.log.levels.WARN,
+                trace_lsp = "off", -- "off" | "messages" | "verbose"
+                trace_lsp_progress = false,
+                log_lsp_messages = false,
+            },
+            copilot_node_command = 'node', -- Node.js version must be > 20
+            workspace_folders = {},
+            copilot_model = "",  -- Current LSP default is gpt-35-turbo, supports gpt-4o-copilot
+            root_dir = function()
+                return vim.fs.dirname(vim.fs.find(".git", { upward = true })[1])
+            end,
+            should_attach = function(_, _)
+                if not vim.bo.buflisted then
+                    logger.debug("not attaching, buffer is not 'buflisted'")
+                    return false
+                end
+
+                if vim.bo.buftype ~= "" then
+                    logger.debug("not attaching, buffer 'buftype' is " .. vim.bo.buftype)
+                    return false
+                end
+
+                return true
+            end,
+            server = {
+                type = "nodejs", -- "nodejs" | "binary"
+                custom_server_filepath = nil,
+            },
+            server_opts_overrides = {},
+        })
+    end,
 }
