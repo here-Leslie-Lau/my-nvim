67e501cc06df61c13bd599dad586779c03a74127
Leslie-Lau <804738464@qq.com>
2025-07-29 16:57:24 +0800

    feat(lsp): add autoformat on save for C files

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,13 +1,2 @@
 require("mason").setup()
 require("mason-lspconfig").setup()
-
-for _, val in ipairs({ "lua_ls", "gopls", "clangd", "taplo", "buf_ls" }) do
-    -- require("lspconfig")[val].setup({
-        -- on_attach = function(client, bufnr)
-        --    if client.server_capabilities.inlayHintProvider then
-        --       vim.lsp.inlay_hint.enable(true)
-        --    end
-        -- end,
-       -- capabilities = require("blink.cmp").get_lsp_capabilities(),
-   -- })
-end
--- lua/plugins/plugin-mason-lspconfig.lua
+++ lua/plugins/plugin-mason-lspconfig.lua
@@ -1,4 +1,12 @@
 return {
     "williamboman/mason-lspconfig.nvim",
     event = "CmdlineEnter",
+    config = function ()
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*.c",
+            callback = function()
+                vim.lsp.buf.format({ async = true })
+            end,
+        })
+    end
 }
