251a71456d9716d33abdb9b89b2439086d39154b
Leslie-Lau <804738464@qq.com>
2025-11-11 14:44:33 +0800

    feat(colorscheme): set random colorscheme on startup

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,3 +1,9 @@
 vim.base16colorspace = 256
-vim.cmd('colorscheme gruvbox')
--- vim.cmd('colorscheme everforest')
+
+local themes = { "gruvbox", "everforest", "everforest" }
+-- make sure the random seed is different each time
+math.randomseed(os.time())
+local random_index = math.random(#themes)
+-- set the colorscheme
+local chosen_theme = themes[random_index]
+vim.cmd("colorscheme " .. chosen_theme)
