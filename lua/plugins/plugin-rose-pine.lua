5873e6c45ead81b8c9583d7e0309f5d886c9d61d
Leslie-Lau <804738464@qq.com>
2025-12-27 14:17:32 +0800

    chore(rose-pine): use moon variant

--- lua/plugins/plugin-rose-pine.lua
+++ lua/plugins/plugin-rose-pine.lua
@@ -5,7 +5,7 @@ return {
     name = "rose-pine",
     config = function()
         require("rose-pine").setup({
-            variant = "main", -- auto, main, moon, or dawn
+            variant = "moon", -- auto, main, moon, or dawn
             dark_variant = "main", -- main, moon, or dawn
             dim_inactive_windows = false,
             extend_background_behind_borders = true,
