c514fd5bb20cfdde724c16d2b726a7198ef41e11
Leslie-Lau <804738464@qq.com>
2026-01-12 12:25:43 +0800

    build(telescope): bump nvim-telescope/telescope.nvim tag to v0.2.1

--- lua/plugins/plugin-telescope.lua
+++ lua/plugins/plugin-telescope.lua
@@ -1,5 +1,5 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    'nvim-telescope/telescope.nvim', tag = 'v0.2.1',
     lazy = true,
     cmd = 'Telescope',  -- when run Telescope command, load this plugin
     -- or                              , branch = '0.1.x',
