36b5b42ea7caf937e864564fbf51a208ed6ce8b1
Leslie-Lau <804738464@qq.com>
2026-03-19 23:32:07 +0800

    feat(lsp): add zls LSP config (cmd=/Users/leslie/.local/bin/zls) and remove lsp_install.sh

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,2 +1,20 @@
 require("mason").setup()
 require("mason-lspconfig").setup()
+
+vim.lsp.config['zls'] = {
+  -- Set to 'zls' if `zls` is in your PATH
+  cmd = { '/Users/leslie/.local/bin/zls' },
+  filetypes = { 'zig' },
+  root_markers = { 'build.zig' },
+
+  -- Further information on how to configure ZLS:
+  -- https://zigtools.org/zls/configure/
+  settings = {
+    zls = {
+      -- Whether to enable build-on-save diagnostics
+      --
+      enable_build_on_save = true,
+    }
+  },
+}
+vim.lsp.enable('zls')
--- shells/lsp_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-file_path="../init.lua"
-
-nvim --headless -c "MasonInstall buf clangd taplo lua-language-server gopls" -c 'wq' "$file_path"
