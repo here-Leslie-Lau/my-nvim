a8ba737d80b690b015f8c95a425216b730ef24a2
Leslie-Lau <804738464@qq.com>
2026-03-23 14:31:12 +0800

    fix(mason-lspconfig): remove *.zig from BufWritePre format-on-save pattern

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -3,7 +3,7 @@ return {
     event = "CmdlineEnter",
     config = function ()
         vim.api.nvim_create_autocmd("BufWritePre", {
-            pattern = "*.c,*.h,*.zig",
+            pattern = "*.c,*.h",
             callback = function()
                 vim.lsp.buf.format({ async = true })
             end,
