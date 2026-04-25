0f5da6ecbcacf97659a0c806e83f329426dbfbe0
Leslie-Lau <804738464@qq.com>
2026-04-25 21:40:31 +0800

    feat: add zig fmt autocmd on BufWritePre

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,6 +1,36 @@
 require("mason").setup()
 require("mason-lspconfig").setup()
 
+local zig_fmt_group = vim.api.nvim_create_augroup("zig_fmt", {})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.zig",
+  group = zig_fmt_group,
+  callback = function(args)
+    local lines = vim.api.nvim_buf_get_lines(args.buf, 0, -1, false)
+    local input = table.concat(lines, "\n")
+
+    if vim.bo[args.buf].endofline then
+      input = input .. "\n"
+    end
+
+    local output = vim.fn.system({ "zig", "fmt", "--stdin" }, input)
+    if vim.v.shell_error ~= 0 then
+      vim.notify(output, vim.log.levels.ERROR)
+      return
+    end
+
+    local formatted = vim.split(output, "\n", { plain = true })
+    if formatted[#formatted] == "" then
+      table.remove(formatted)
+    end
+
+    local view = vim.fn.winsaveview()
+    vim.api.nvim_buf_set_lines(args.buf, 0, -1, false, formatted)
+    vim.fn.winrestview(view)
+  end,
+})
+
 vim.lsp.config['zls'] = {
   -- Set to 'zls' if `zls` is in your PATH
   cmd = { '/Users/leslie/.local/bin/zls' },
